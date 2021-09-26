5a980a7b9edd1136177cfde979f425811e745d14
pranav-dabre <79563128+pranav-dabre@users.noreply.github.com>
2021-09-26 18:54:01 +0530

    js change

--- js/demo1.js
+++ js/demo1.js
@@ -3,7 +3,7 @@ var width = canvas.offsetWidth,
     height = canvas.offsetHeight;
 
 var colors = [
-    new THREE.Color(0xac1122),
+    new THREE.Color(0x19dfbe),
     new THREE.Color(0x96789f),
     new THREE.Color(0x535353)];
 
